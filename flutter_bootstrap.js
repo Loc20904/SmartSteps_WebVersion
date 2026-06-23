cb93f39860b200f1e00a785c8be0b67ae51b92c2
loc20904 <nguyenthanhloc20092004@gmail.com>
2026-06-23 14:04:48 +0700

    Fix: Update UX parents dashboard

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "124683430" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "487703992" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
