c2f81d722f336d88123be1c150333e39dbc22936
loc20904 <nguyenthanhloc20092004@gmail.com>
2026-07-02 13:00:59 +0700

    fix: enhance UI

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2456551301" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1710278750" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
